a379985d3b82974e325417cfe21cc7d9e391de63
augutis44 <kraujalytee@gmail.com>
2023-07-16 01:12:10 +0300

    Added Contacts page

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,43 +1,63 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Survey = () => {
 
     return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-                
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <div>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Required"
-                    defaultValue="Vardas"
-                />
-                <TextField
-                    required
-                    id="outlined-required"
-                    defaultValue="Pavardė"
-                />
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Required"
-                    defaultValue="El.paštas"
-                />
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Required"
-                    defaultValue="Klausimas"
-                />
-            </div>
-        </Box>
+        <>
+            <Typography variant="h2" sx={{
+                textAlign: 'center'
+            }}>
+                SUSISIEKITE
+            </Typography>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { margin: '0.5rem 0rem', width: '100%' },
+                    marginBottom: '3rem',
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Box sx={{
+                    padding: '0rem 15rem'
+                }}>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                    }}>
+                        <TextField
+                            required
+                            label="Privalomas"
+                            defaultValue="Vardas"
+                        />
+                        <TextField
+                            defaultValue="Pavardė"
+                        />
+                        <TextField
+                            required
+                            label="Privalomas"
+                            defaultValue="El.paštas"
+                        />
+                        <TextField
+                            required
+                            id="outlined-multiline-static"
+                            label="Privalomas"
+                            multiline
+                            rows={4}
+                            defaultValue="Klausimas"
+                        />
+                        <Button variant="contained" color="secondary" sx={{
+                            color: '#ffffff'
+                        }}>
+                            Susisiekti
+                        </Button>
+                    </Box>
+
+                </Box>
+            </Box >
+
+        </>
     )
 }
 
