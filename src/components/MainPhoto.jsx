40dd8233edae847eee834caf38bf02f05704f4d1
augutis44 <kraujalytee@gmail.com>
2023-08-02 23:57:10 +0300

    redid MainPhoto component and made it responsive

--- src/components/MainPhoto.jsx
+++ src/components/MainPhoto.jsx
@@ -1,50 +1,37 @@
-import { Box, Card, Typography } from '@mui/material';
+import { Card, Typography, useMediaQuery } from '@mui/material';
 import RegitaLogo from '../icons/RegitaLogo';
+import { useTheme } from '@mui/material/styles';
 
 const MainPhoto = () => {
+    const theme = useTheme();
+    const isDownSm = useMediaQuery(theme.breakpoints.down('md'));
 
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-        }}
+        <Card
+            sx={{
+                backgroundImage: isDownSm ? "url('public/pexels-andrea-piacquadio-3770194 - Copy.jpg')" : "url('public/pexels-andrea-piacquadio-3770194.jpg')",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundAttachment: 'fixed',
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column'
+            }}
         >
-            <Card
+            <RegitaLogo variant='white' width="21rem" height="6.5rem" />
+            <Typography
                 sx={{
-                    backgroundImage: "url('public/pexels-andrea-piacquadio-3770194.jpg')",
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    backgroundAttachment: 'fixed',
-                    height: '100vh',
+                    fontSize: '30px',
+                    color: 'white',
+                    margin: '1rem',
+                    textAlign: 'center'
                 }}
             >
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        alignItems: 'center',
-                        top: '20rem',
-                        left: '30rem',
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}
-                >
-                    <Box>
-                        <RegitaLogo variant='white' width="21rem" height="6.5rem" />
-                    </Box>
-                    <Box>
-                        <Typography
-                            sx={{
-                                fontSize: '30px',
-                                color: 'white'
-                            }}
-                        >
-                            Jūsų patikimas vadovas finansų pasaulyje
-                        </Typography>
-                    </Box>
-                </Box>
-
-            </Card >
-        </Box>
+                Jūsų patikimas vadovas finansų pasaulyje
+            </Typography>
+        </Card >
     );
 }
 
