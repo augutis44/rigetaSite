97c180dfa234f93f6530cdd1269e0ee9d884d375
augutis44 <kraujalytee@gmail.com>
2023-08-03 19:57:58 +0300

    Redid services cards and made them responsive

--- src/components/ServicesMain.jsx
+++ src/components/ServicesMain.jsx
@@ -1,73 +1,68 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography, useMediaQuery } from "@mui/material";
 import { SideBox } from "../helpers/StyledBox";
-import Buttons from "./Buttons";
+import { useTheme } from '@mui/material/styles';
+import ServiceHomeCard from "./ServiceHomeCard";
+import { Link } from "react-router-dom";
+
 
 const ServicesMain = () => {
+    const theme = useTheme();
+    const isDownSm = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
         <SideBox>
-            <Box sx={{
-                align: 'center',
-                margin: 'auto',
+            <Typography variant={isDownSm ? 'h3' : 'h2'} sx={{
+                marginLeft: '1rem',
+                marginBottom: '2rem',
+                fontWeight: '400'
             }}>
-                <Typography variant='h3' sx={{
-                    margin: '0rem 0rem 2rem 1rem'
-                }}>
-                    Paslaugos
-                </Typography>
-                <Grid container spacing={2} sx={{
-                    display: 'flex',
-                    flexWrap: 'nowrap'
-                }}>
-                    <Grid item>
-                        <Card>
-                            <CardMedia
-                                component="img"
-                                maxHeight="140"
-                                image="public\flat-lay-payroll-concept-with-document.jpg"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Buhalterinės paslaugos
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Curabitur et turpis vestibulum, ultricies urna vitae, lacinia est.
-                                    Maecenas ac velit metus. Nunc tincidunt, libero at tincidunt  turpis.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Buttons type="red">Sužinoti daugiau</Buttons>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item>
-                        <Card >
-                            <CardMedia
-                                component="img"
-                                maxHeight="140"
-                                image="public\flat-lay-payroll-concept-with-document.jpg"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Projektų paraiškų pildymas
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Curabitur et turpis vestibulum, ultricies urna
-                                    vitae, lacinia est. Maecenas ac velit metus.
-                                    Nunc tincidunt, libero at tincidunt  turpis
-                                    vestibulum, ultricies urna.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Buttons type="red">Sužinoti daugiau</Buttons>
-                            </CardActions>
-                        </Card>
-                    </Grid>
+                Paslaugos
+            </Typography>
+            <Grid container spacing={2} sx={{
+                display: 'flex',
+                flexWrap: 'nowrap',
+                flexDirection: { xs: 'column', md: 'row' }
+            }}>
+                <Grid item>
+                    <ServiceHomeCard title={'Buhalterinės paslaugos'}>
+                        Curabitur et turpis vestibulum, ultricies urna vitae, lacinia est.
+                        Maecenas ac velit metus. Nunc tincidunt, libero at tincidunt  turpis.
+                    </ServiceHomeCard>
                 </Grid>
+                <Grid item>
+                    <ServiceHomeCard title={'Projektų paraiškų pildymas'}>
+                        Curabitur et turpis vestibulum, ultricies urna
+                        vitae, lacinia est. Maecenas ac velit metus.
+                        Nunc tincidunt, libero at tincidunt  turpis
+                        vestibulum, ultricies urna.
+                    </ServiceHomeCard>
+                </Grid>
+            </Grid>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '2rem'
+            }}>
+                <Link to='/services'>
+                    <Button sx={{
+                        backgroundColor: '#E63946',
+                        color: '#FFFFFF',
+                        padding: '0rem 2rem',
+                        fontSize: '2rem',
+                        textTransform: 'capitalize',
+                        boxShadow: '3',
+                        fontWeight: '400',
+                        '&:hover': {
+                            backgroundColor: '#f8847f',
+                            color: '#FFFFFF',
+                        }
+                    }}>
+                        Visos paslaugos
+                    </Button>
+                </Link>
             </Box>
         </SideBox>
-    )
+    );
 }
 
 export default ServicesMain;
-
-
