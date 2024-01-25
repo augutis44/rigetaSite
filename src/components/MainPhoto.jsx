4054e66ee102f2f904618a1c36c58f840a97ad14
augutis44 <kraujalytee@gmail.com>
2024-01-26 00:13:12 +0200

    header changes

--- src/components/MainPhoto.jsx
+++ src/components/MainPhoto.jsx
@@ -6,21 +6,25 @@ const MainPhoto = () => {
     const theme = useTheme();
     const isDownMd = useMediaQuery(theme.breakpoints.down('md'));
 
+    const logoHeight = isDownMd ? '16rem' : "21rem"
+
     return (
         <Card
             sx={{
-                backgroundImage: isDownMd ? "url('/pexels-andrea-piacquadio-3770194 - Copy.jpg')" : "url('/pexels-andrea-piacquadio-3770194.jpg')",
+                background: isDownMd ?
+                    'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("/pexels-andrea-piacquadio-3770194 - Copy.jpg")'
+                    : 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("pexels-andrea-piacquadio-3770194.jpg")'
+                ,
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
-                backgroundAttachment: 'fixed',
-                height: '100vh',
+                height: '80vh',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 flexDirection: 'column'
             }}
         >
-            <RegitaLogo variant='white' width="21rem" height="6.5rem" />
+            <RegitaLogo variant='white' width={logoHeight} height="6.5rem" />
             <Typography
                 sx={{
                     fontSize: '30px',
